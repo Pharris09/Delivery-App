2f845ec9e936854f76dc99207f134a732f89885b
Pharris09 <pkoumdanani@gmail.com>
2022-09-03 22:23:06 +0100

    Bottom navigation bar (both principale and secondary) with fragments for principale only

--- app/src/main/java/com/example/deliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryapp/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.deliveryapp
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.deliveryapp.databinding.ActivityMainBinding
+import com.example.deliveryapp.ui.HomeFragment
+import com.example.deliveryapp.ui.PanierFragment
+import com.example.deliveryapp.ui.ParcourirFragment
+import com.example.deliveryapp.ui.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var currentFragment : Fragment
+    private lateinit var bottomNavView : BottomNavigationView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        bottomNavView = binding.navigationBarMainActivity
+        bottomNavView.setOnNavigationItemSelectedListener(navListener)
+    }
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener{
+        when(it.itemId){
+            R.id.navigation_home -> {
+                currentFragment = HomeFragment()
+            }
+            R.id.navigation_parcourir -> {
+                currentFragment = ParcourirFragment()
+            }
+            R.id.navigation_panier -> {
+                currentFragment = PanierFragment()
+            }
+            R.id.navigation_settings -> {
+                currentFragment = SettingsFragment()
+            }
+        }
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame,currentFragment)
+            commit()
+        }
+        true
     }
 }
